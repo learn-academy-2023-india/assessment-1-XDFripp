63a740c5cb4c72ca345de4335fc86656a213a737
xdfripp@gmail.com <learnacademy@LEARNs-MacBook-Air-4.local>
2023-11-17 16:34:44 -0800

    answered some questions

--- code-challenges.js
+++ code-challenges.js
@@ -25,16 +25,27 @@ const fruit4 = "kiwi"
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+// console.log((padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns)).length)
 // Expected output: 9
 
-// Pseudo code:
+// Pseudo code: 
+//  Input: combine both const arrays 
+//  Output: after combined they should equal the expected output which is 9
+//  What we want to do is combine both const.  To do so, we would .concat(padres1998worldSeriesRuns) to the end of padres1994WorldSeriesRuns.
+//  Once that is done, we have to .length at the end of all of that to get the number output, otherwise the terminal would just kick out
+//  all 9 numbers in a combined array.
+
 
 // --------------------2) Create the code that will reverse the letters of a string using the test variable provided below.
 
 const currentCohort = "LEARN 2023"
+// console.log((currentCohort.split()).reverse())
 // Expected output: "3202 NRAEL"
 
 // Pseudo code:
+//  Input: 
+//  Output:
+
 
 // --------------------3) Use a for loop to log only the odd numbers from the array using the test variable provided below.
 
